be583f5be657d2db20966d5d4c64a4c4b22a14c2
leofdss <leonardo.f.desouzasilva@gmail.com>
2019-03-10 21:04:37 -0300

    first commit

---
+++ arvore.py
@@ -0,0 +1,29 @@
+class Node(object):
+    def __init__(self, key):
+        self.key = key
+        self.children = None
+        self.dad = ''
+
+    def BFS(self, key):
+        print('\n-----------Busca-----------')
+        return BFS([self], key)
+
+
+def BFS(nodes, key):
+    filhos = []
+    if nodes:
+        for item in nodes:
+            print(item.key)
+            if item.key == key:
+                print('-----------Fim da Busca-----------')
+                return item
+            elif item.children:
+                i = 0
+                while i < len(item.children):
+                    item.children[i].dad = item.key
+                    i += 1
+                filhos += item.children
+        print('Proxima camada')
+        return BFS(filhos, key)
+    else:
+        return None
---
+++ main.py
@@ -0,0 +1,38 @@
+from arvore import *
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g1 = Node('g')
+g2 = Node('g')
+h = Node('h')
+
+c.children = [Node('i')]
+d.children = [Node('i')]
+b.children = [c, d]
+g1.children = [Node('i')]
+g2.children = [Node('i')]
+f.children = [g1]
+e.children = [f, g2]
+h.children = [Node('i')]
+a.children = [h, e, b]
+
+'''
+  a
+/ |  \ 
+h e   b
+| |\  |\ 
+i f g c d
+  | | | |
+  g i i i
+  |
+  i
+'''
+
+result = a.BFS('g')
+
+print('Resultado: ' + result.key)
+print('Pai: ' + result.dad)
